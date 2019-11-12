e710084e6a3c657feedb8d0b7af14908e32d2059
alexvancap <alexvancap@gmail.com>
2019-11-12 17:00:37 -0600

    add password collumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_224343) do
+ActiveRecord::Schema.define(version: 2019_11_12_224510) do
 
   create_table "games", force: :cascade do |t|
     t.string "venue"
