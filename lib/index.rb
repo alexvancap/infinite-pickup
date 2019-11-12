844151acb9df4fa745c9d64b9ec616f0b4014d6b
alexvancap <alexvancap@gmail.com>
2019-11-11 18:14:22 -0600

    it's working mate

--- lib/index.rb
+++ lib/index.rb
@@ -1,4 +1,4 @@
-
+ActiveRecord::Base.logger = nil
 $prompt = TTY::Prompt.new
 $current_user = nil
 
@@ -11,18 +11,19 @@ def login
     puts "Please enter your full name..."
     puts "\n"
     name = gets.chomp.downcase.to_s
-
+    is_player = 0
 
     Player.all.each do |player|
         if player.name == name
-            p "Welcome #{name}!"
-            return $current_user = player
-        else
-            Player.create({name: name})
-            p "Welcome to our platform #{name}"
-            return $current_user = player
+            $current_user = player
+            is_player = 1
+            return puts "Welcome back #{name}!"
         end
-        puts "fuck"
+    end
+
+    if is_player = 0
+        Player.create({name: name})
+        puts "Welcome to our platform #{name}"
     end
 end
 
@@ -37,5 +38,8 @@ def dashboard
         join_a_game
     elsif selection == "Your games"
         your_games
+    else
+        puts "\n"
+        puts "Bye!"
     end
 end
