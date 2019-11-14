494bccfc8119b551dfb076935cfef4b8cb8b6964
alexvancap <alexvancap@gmail.com>
2019-11-14 14:38:10 -0600

    fixed bugs

--- lib/your_games.rb
+++ lib/your_games.rb
@@ -16,40 +16,39 @@ def joined_games
         if back_button == "Back"
            your_games
         end
-
 end
+
+
+def leave_a_game
+    
+end
+
 
 def created_games
     selection = $prompt.select("Choose your option".blue, ["Full", "Not Full", "Back"])
 
     if selection == "Full"
         array = Game.all.select do |game|
-            game.max_player <= game.joined_players
+            (game.creator == $current_user.id.to_) && (game.max_player == game.joined_players)
         end
-    
+
         array.map do |game|
             puts "#{game.game_type}, #{game.venue}, #{game.date}, #{game.time}".blue
         end
 
-        back_button = $prompt.select("press back to go back to the menu".blue, ["Back"])
-        if back_button == "Back"
-           created_games
-        end
+        back_button(created_games) #lib/helper_methods.rb
     
     elsif selection == "Not Full"
 
         result = Game.all.select do |game|
-            game.creator == $current_user.id.to_s  
+            (game.creator == $current_user.id.to_s) && (game.max_player > game.joined_players)
         end
 
         result.map do |game|
             puts "#{game.game_type}, #{game.venue}, #{game.date}, #{game.time}".blue
         end
 
-        back_button = $prompt.select("press back to go back to the menu".blue, ["Back"])
-        if back_button == "Back"
-           created_games
-        end
+        back_button(created_games)
     
     elsif selection == "Back"
         your_games
@@ -72,13 +71,15 @@ def your_games
     else
         puts "\n"
 
-        selection = $prompt.select("What games would you like to view?".blue, ["Joined Games", "Games You've Created", "Back"])
+        selection = $prompt.select("What games would you like to view?".blue, ["Leave A Game", "View Joined Games", "View Games You've Created", "Back"])
         
         puts "\n"
 
-        if selection == "Joined Games"
+        if selection == "Leave A Game"
+            leave_a_game
+        elsif selection == "View Joined Games"
             joined_games
-        elsif selection == "Games You've Created"
+        elsif selection == "View Games You've Created"
             created_games
         elsif selection == "Back"
             dashboard
