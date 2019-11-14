a5b487676f6b3db829477e2d398ec4a7982ef799
Joseph Burks <joe_burks_3@yahoo.com>
2019-11-14 11:25:47 -0600

    Validate user input date

--- lib/create_a_game.rb
+++ lib/create_a_game.rb
@@ -1,25 +1,23 @@
+$date = nil
 def create_a_game
 
     puts "\n"
     user_sport = $prompt.ask("What sport would you like to organize?".blue)
     sport = (user_sport).split(/ |\_/).map(&:capitalize).join(" ")
-
+    puts "\n"
     max_players = $prompt.ask("How many players may join your game?".blue)
-    
     puts "\n"
     user_venue = $prompt.ask("Where would you like to play?".blue)
     venue = (user_venue).split(/ |\_/).map(&:capitalize).join(" ")
     puts "\n"
-    date = $prompt.ask("Please enter the date you would like to play:".blue) #do |i|
-#        i.validate(\, "Please enter your date in mm/dd/yyyy format.")
-#    end
+    ask_for_date # lib/date.rb
     puts "\n"
     time = $prompt.ask("Please enter the time you would like to play:".blue)
     puts "\n"
-    new_game = Game.create({venue: venue, game_type: sport, creator: $current_user.id, time: time, date: date, joined_players: 1, max_player: max_players})
+    new_game = Game.create({venue: venue, game_type: sport, creator: $current_user.id, time: time, date: $date, joined_players: 1, max_player: max_players})
     
-    if Game.find_by!(venue: venue, game_type: sport, time: time, date: date)
-        user_yes_no = $prompt.select("Are you sure you would like to create this pickup game?".yellow, ["For sure!", "Nah, I'm just foolin'","Nah, I messed up!"])
+    if Game.find_by!(venue: venue, game_type: sport, time: time, date: $date)
+        user_yes_no = $prompt.select("Are you sure you would like to create this pickup game?".yellow, ["For sure!", "Nah, I'm just foolin'","Nooo, I messed up!"])
         if user_yes_no == "For sure!"
             Matchup.create({player: $current_user, game: new_game})
             puts "\nYour #{new_game.game_type.downcase} game at #{new_game.venue} on\n#{new_game.date} at #{new_game.time} has been successfully created.".green
