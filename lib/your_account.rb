d924ffa69085b69c79284a34ff3f6a0ff97ba018
Joseph Burks <joe_burks_3@yahoo.com>
2019-11-15 10:13:13 -0600

    your account bug fixes

--- lib/your_account.rb
+++ lib/your_account.rb
@@ -1,18 +1,18 @@
 def change_username
     puts "\n"
     puts "#{"Please enter your".blue} #{"new".bold.blue} #{"username".blue}"
-    puts "#{"type".blue} #{"exit".bold.blue} #{"to go to the menue".blue}"
+    puts "#{"Type".blue} #{"exit".bold.blue} #{"to go to the dashboard.".blue}"
     new_username = gets.chomp.downcase.to_s
 
     if new_username == "exit"
-        puts "sending you back to the menue.".yellow
+        puts "Sending you back to the dashboard.".yellow
         dashboard
 
     else
         Player.all.each do |player|
             if player.id == $current_user.id
                 player.update({username: new_username})
-                puts "#{"Your username has been succesfully changed to".green} #{"new_username".bold.green}"
+                puts "#{"Your username has been succesfully changed to".green} #{new_username.bold.green}"
                 puts "\n"
                 dashboard
             end
@@ -24,7 +24,7 @@ end
 def change_password
     puts "\n"
     puts "#{"Please enter".blue} #{"old".bold.blue} #{"password".blue}"
-    puts "#{"type".blue} #{"exit".bold.blue} #{"to go to the menue".blue}"
+    puts "#{"Type".blue} #{"exit".bold.blue} #{"to go to the dashboard.".blue}"
     old_pass = gets.chomp
 
     Player.all.each do |player|
@@ -34,7 +34,7 @@ def change_password
             if player.password == old_pass
                 #checks if the same password was not entered twice
                 puts "#{"Please enter your".blue} #{"new".bold.blue} #{"password".blue}"
-                puts "#{"type".blue} #{"exit".bold.blue} #{"to go to the menue".blue}"
+                puts "#{"Type".blue} #{"exit".bold.blue} #{"to go to the dashboard.".blue}"
                 new_pass = gets.chomp
 
                 if new_pass != old_pass
@@ -45,16 +45,12 @@ def change_password
                     puts "\n"
                     puts "Your password has been changed".green
                     dashboard
-
-                elsif new_pass == "exit"
-                    puts "sending you back to the menue.".yellow
-                    dashboard
                 else
-                    puts "Please don't enter the same password as your old password!".red
+                    puts "Your new password cannot be the same as your old password!".red
                     change_password
                 end
             elsif old_pass == "exit"
-                puts "sending you back to the menue.".yellow
+                puts "Sending you back to the dashboard.".yellow
                 dashboard
             else
                 puts "The entered password does not match your old password!".red
@@ -66,7 +62,7 @@ end
 
 def your_account
     
-    chosen_option = $prompt.select("Please select your option".blue, ["Change username", "Change password", "Back"])
+    chosen_option = $prompt.select("Please select an option:".blue, ["Change username", "Change password", "Back"])
 
     if chosen_option == "Change username"
         change_username
